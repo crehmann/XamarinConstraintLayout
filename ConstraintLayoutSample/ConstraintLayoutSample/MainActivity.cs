206f881c2ea7da39845b209c2c860a279145e809
Christoph Rehmann <christoph.rehmann@noser.com>
2016-12-06 22:09:21 +0100

    updated layout

--- ConstraintLayoutSample/ConstraintLayoutSample/MainActivity.cs
+++ ConstraintLayoutSample/ConstraintLayoutSample/MainActivity.cs
@@ -1,18 +1,15 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
 
 namespace ConstraintLayoutSample
 {
-    [Activity(Label = "ConstraintLayoutSample", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
         }
     }
 }
